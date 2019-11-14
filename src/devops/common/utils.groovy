dd67faa13d0667db1f4273fd1364676fcf95925e
Matt Schuchard <grapheneowns@gmail.com>
2019-11-14 16:18:59 -0500

    add method to help bridge gap between DSLs

--- src/devops/common/utils.groovy
+++ src/devops/common/utils.groovy
@@ -46,3 +46,28 @@ void download_file(String url, String dest) {
 String map_to_json(Map content) {
   return JsonOutput.toJson(content);
 }
+
+// converts closure body to config map, or returns same config map
+// bridges gap between users of older DSL and newer DSL
+Map params_converter(body) {
+  // initialize config
+  Map config = [:]
+
+  // if we received older DSL, convert it into config map
+  if (body instanceof Closure) {
+    // evaluate the body block and collect configuration into the object
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+  }
+  // if newer DSL, return same map as the config
+  else if (body instanceof Map) {
+    config = body
+  }
+  // params are invalid type
+  else {
+    throw new Exception('The parameter inputs are an invalid type. They must either be a Closure or Map. Consult the documentation for more information.')
+  }
+
+  return config
+}
