00d0a6a4c76729624fd94eb337470d17d64d279b
Matt Schuchard <grapheneowns@gmail.com>
2021-02-12 10:29:19 -0500

    update task method to utilize httprequest plugin

--- vars/puppet.groovy
+++ vars/puppet.groovy
@@ -34,10 +34,10 @@ void code_deploy(body) {
   // convert map to json string
   payload = new utils().mapToJSON(payload)
 
-  // initialize errored var
+  // initialize vars
   boolean errored = false
-  // initialize json_response object
   def json_response = [:]
+  Map response = [:]
   // initialize token with readFile relative pathing requirement stupidness
   String token = readFile("../../../../../../../../../../../${config.token}")
 
@@ -63,7 +63,7 @@ void code_deploy(body) {
     }
     // parse response
     try {
-      Map response = readJSON(text: json_response.content)
+      response = readJSON(text: json_response.content)
     }
     catch(Exception error) {
       print "Response from ${server} is not valid JSON! Response content: ${json_response.content}."
@@ -152,20 +152,39 @@ void task(body) {
   // convert map to json file
   payload = new utils().mapToJSON(payload)
 
+  // initialize vars
+  def json_response = [:]
+  Map response = [:]
+  // initialize token with readFile relative pathing requirement stupidness
+  String token = readFile("../../../../../../../../../../../${config.token}")
+
   // trigger task orchestration
   try {
-    String json = sh(label: 'Puppet Task Request', returnStdout: true, script: "${config.bin} -k -X POST -H 'Content-Type: application/json' -H \"X-Authentication: `cat ${config.token}`\" \"https://${server}:8143/orchestrator/v1/command/task\" -d '${payload}'")
+    // trigger code manager deployment
+    try {
+      json_response = httpRequest(
+        acceptType:             'APPLICATION_JSON',
+        consoleLogResponseBody: true,
+        contentType:            'APPLICATION_JSON',
+        customHeaders:          [[name: 'X-Authentication', value: token]],
+        httpMode:               'POST',
+        ignoreSslErrors:        true,
+        quiet:                  true,
+        requestBody:            payload,
+        url:                    "https://${server}:8143/orchestrator/v1/command/task",
+      )
+    }
   }
   catch(Exception error) {
-    print "Failure executing curl against ${server} with token at ${config.token}!"
+    print "Failure executing REST API request against ${server} with token at ${config.token}! Returned status: ${json_response.status}."
     throw error
   }
   // receive and parse response
   try {
-    Map response = readJSON(text: json)
+    Map response = readJSON(text: json.content)
   }
   catch(Exception error) {
-    print "Response from ${server} is not valid JSON!"
+    print "Response from ${server} is not valid JSON! Response content: ${json_response.content}."
     throw error
   }
   // handle errors in response
