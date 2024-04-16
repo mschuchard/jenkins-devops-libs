960eac919d79a5f3d7683d5fdb71c417cdb07ac6
Matt Schuchard <grapheneowns@gmail.com>
2024-04-16 11:00:46 -0400

    improve and fix hclparser wrapper

--- src/devops/common/hcl.groovy
+++ src/devops/common/hcl.groovy
@@ -2,18 +2,33 @@
 package devops.common;
 
 // imports
-@Grab('com.bertramlabs.plugins:hcl4j:0.7.3')
-import com.bertramlabs.plugins.hcl4j.HCLParser;
+import jenkins.model.Jenkins
+@Grab('com.bertramlabs.plugins:hcl4j:0.9.1')
+import com.bertramlabs.plugins.hcl4j.HCLParser
 
 // wrapper method for returning a map from a hcl file
 Map hclToMap(String filePath) {
+  def file = null;
+
   // load the file from the jenkins master
   if (env['NODE_NAME'].equals('master')) {
+    // verify file exists
+    if !(File(filePath).exists()) {
+      print "File does not exist at ${filePath}"
+      throw new FileNotFoundException("HCL file does not exist")
+    }
+
     final file = new File(filePath);
   }
   // load the file from the build agent/node
   else {
-    final file = new FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), filePath)
+    // verify file exists
+    if !(FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), dir).exists()) {
+      print "File does not exist at ${filePath}"
+      throw new FileNotFoundException("HCL file does not exist")
+    }
+
+    final file = new FilePath(Jenkins.getInstance().getComputer(env['NODE_NAME']).getChannel(), filePath);
   }
 
   // return map from parsed hcl-formatted string
